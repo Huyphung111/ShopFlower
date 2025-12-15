855aea5db281457f86977cf00105eefa7faef3c4
Phùng Tuấn Huy <huythelep@gmail.com>
2025-12-15 07:59:04 +0700

    Add project files.

---
+++ ShopFlower/App_Start/WebApiConfig.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Web.Http;
+
+namespace ShopFlower
+{
+    public class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            // Route mặc định
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            //// Remove the XML formatter to default to JSON
+            //config.Formatters.Remove(config.Formatters.XmlFormatter);
+
+            //// Optional: Make JSON the default for browsers
+            //config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+
+        }
+    }
+}
